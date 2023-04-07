30988a3c064025fdad2aa6a915ff5973bcd74253
Alissa <alissa.werbach@live.de>
2023-04-08 00:19:38 +0200

    added loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,9 @@
+import { Loader } from './Loader/Loader';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    ></div>
+    <div>
+      <Loader />
+    </div>
   );
 };
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import css from './Loader.module.css';
+
+export const Loader = () => {
+  return (
+    <div className={css.loader}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
