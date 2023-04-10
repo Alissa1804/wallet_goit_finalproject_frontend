3106f33253ef2ce5729bbd25e78c5ed1c13a16de
Kamila Nauryzbayeva <kamila.nryzbv@gmail.com>
2023-04-10 21:07:31 +0600

    Feature/registrationForm
    Форма регистрации (верстка мобильная и десктоп, redux, yup), страница регистрации

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
-import { Loader } from './Loader/Loader';
-import { Header } from './Header/header';
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import { Loader } from './Loader/Loader';
+// import { Header } from './Header/header';
+import { RegistrationPage } from "pages/RegistrationPage/RegistrationPage";
+
+import { PublicRoute } from "HOCs/PublicRoute";
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <Loader />
-    </div>
+    // <div>
+    //   <Header />
+    //   <Loader />
+    // </div>
+    <>
+    <Routes>
+    <Route path="/" element={<PublicRoute redirectTo='/' component={<RegistrationPage/>}/>} />
+  </Routes>
+  <ToastContainer position='bottom-right' />
+    </>
   );
 };
