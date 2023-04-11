940cc533089ac0ffa15fabb22557345efc3d8493
Alissa <alissa.werbach@live.de>
2023-04-11 17:27:41 +0200

    bugfix/fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,20 @@
-feature/add-redux-auth
 //import { Loader } from './Loader/Loader';
 //import { Header } from './Header/header';
-import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // import { fetchCurrentUser } from 'redux/auth/auth-operations';
 // import { useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { PrivateRoute } from 'HOCs/PrivateRoute';
 // import { RestrictedRoute } from 'HOCs/RestrictedRoute';
-import { RegistrationPage } from "pages/RegistrationPage/RegistrationPage";
+import { RegistrationPage } from 'pages/RegistrationPage/RegistrationPage';
 
-import { PublicRoute } from "HOCs/PublicRoute";
+import { PublicRoute } from 'HOCs/PublicRoute';
 
 // import { useAuth } from 'hooks/useAuth';
 
 export const App = () => {
-
   //-----для авторизации-----
   // const dispatch = useDispatch();
 
@@ -30,17 +28,21 @@ export const App = () => {
   //   <b>Refreshing user...</b>
   // ) : (
   //   <>
- return (
+  return (
     // <div>
     //   <Header />
     //   <Loader />
     // </div>
     <>
-    <Routes>
-    <Route path="/" element={<PublicRoute redirectTo='/' component={<RegistrationPage/>}/>} />
-  </Routes>
-  <ToastContainer position='bottom-right' />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PublicRoute redirectTo="/" component={<RegistrationPage />} />
+          }
+        />
+      </Routes>
+      <ToastContainer position="bottom-right" />
     </>
-    main
   );
 };
