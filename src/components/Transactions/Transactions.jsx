6a1a5c414ac953425e4fcd1a0701ccce85f05b23
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-04-13 03:46:53 +0600

    v2

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import styles from './Transactions.module.css';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 
 function Transactions() {
   return (
     <div className={styles.transactions}>
       <ul className={styles.transactions__list}>
         <li className={styles.transactions__item}>
-          <div>
-            <div>Date</div>
-            <div>04.01.19</div>
+          <div className={styles.transactions__row}>
+            <div className={styles.transactions__th}>Date</div>
+            <div className={styles.transactions__tb}>04.01.19</div>
           </div>
-          <div>
-            <div>Type</div>
-            <div>-</div>
+          <div className={styles.transactions__row}>
+            <div className={styles.transactions__th}>Type</div>
+            <div className={styles.transactions__tb}>-</div>
           </div>
-          <div>
-            <div>Category</div>
-            <div>Other</div>
+          <div className={styles.transactions__row}>
+            <div className={styles.transactions__th}>Category</div>
+            <div className={styles.transactions__tb}>Other</div>
           </div>
-          <div>
-            <p>Comment</p>
-            <div>Gift for your wife</div>
+          <div className={styles.transactions__row}>
+            <div className={styles.transactions__th}>Comment</div>
+            <div className={styles.transactions__tb}>Gift for your wife</div>
           </div>
-          <div>
-            <div>Sum</div>
-            <div>300.00</div>
+          <div className={styles.transactions__row}>
+            <div className={styles.transactions__th}>Sum</div>
+            <div className={styles.transactions__red}>300.00</div>
           </div>
-          <button>Delete</button>
-          <button>Edit</button>
+          <div className={styles.transactions__row}>
+            <div>
+              <button className={styles.transactions__btn_d}>Delete</button>
+            </div>
+            <div className={styles.transactions__button_edit}>
+              <div>
+                <ModeEditOutlineOutlinedIcon />
+              </div>
+              <button className={styles.transactions__btn_e}>Edit</button>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -6,11 +6,16 @@ import Transactions from 'components/Transactions/Transactions';
 
 function DashboardPage() {
   return (
-    <div>
+    <div className={styles.dashboard}>
       <header>Header</header>
-      <div className={styles.dashboard}>
-        <Navigation />
-        <Balance amount="24 000.00" />
+      <div className={styles.dashboard__content}>
+        <div className={styles.dashboard__navigation}>
+          <Navigation />
+        </div>
+        <div className={styles.dashboard__balance}>
+          <Balance amount="24 000.00" />{' '}
+        </div>
+
         <Transactions />
         <div>Currency</div>
       </div>
