44553ac0b078ca375d0bc3f5afc0b7f43a9728fa
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-04-14 04:17:02 +0600

    front mobile done

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -4,20 +4,34 @@ import Navigation from 'components/Navigation/Navigation';
 import Balance from 'components/Balance/Balance';
 import Transactions from 'components/Transactions/Transactions';
 import { Header } from '../../components/Header/Header';
+import Currency from 'components/Currency/Currency';
+import AddIcon from '@mui/icons-material/Add';
+import { useDeviceSize } from 'hooks/useDeviceSize';
 
 function DashboardPage() {
+  const { deviceType } = useDeviceSize();
   return (
     <div className={styles.dashboard}>
       <Header />
       <div className={styles.dashboard__content}>
-        <div className={styles.dashboard__navigation}>
-          <Navigation />
+        <div className={styles.dashboard__left}>
+          <div className={styles.dashboard__navigation}>
+            <Navigation />
+          </div>
+          <div className={styles.dashboard__balance}>
+            <Balance amount="24 000.00" />{' '}
+          </div>
         </div>
-        <div className={styles.dashboard__balance}>
-          <Balance amount="24 000.00" />{' '}
+        <div className={styles.dashboard__right}>
+          <div className={styles.dashboard__currency}>
+            <Currency />
+          </div>
         </div>
+
         <Transactions />
-        <div>Currency</div>
+        <button className={styles.dashboard__add}>
+          <AddIcon fontSize="large" />
+        </button>
       </div>
     </div>
   );
