d65b14e6d44e04bf20482d3632000c7157587d31
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-04-15 05:43:38 +0600

    desctop version done

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -12,15 +12,6 @@ function Currency() {
     fetchData();
   }, []);
 
-  if (!currencyData) {
-    return <div>Loading...</div>;
-  }
-
-  const eurBuy = currencyData.eur.buy;
-  const eurSale = currencyData.eur.sale;
-  const USDSale = currencyData.usd.sale;
-  const USDBuy = currencyData.usd.buy;
-
   return (
     <div className={styles.currency}>
       <table className={styles.currency__tbl}>
@@ -34,18 +25,53 @@ function Currency() {
         <tbody className={styles.currency__tbody}>
           <tr>
             <td className={styles.currency__tbl_item}>USD</td>
-            <td className={styles.currency__tbl_item}>{USDBuy}</td>
-            <td className={styles.currency__tbl_item}>{USDSale}</td>
+            <td className={styles.currency__tbl_item}>5.5</td>
+            <td className={styles.currency__tbl_item}>5.5</td>
           </tr>
           <tr>
             <td className={styles.currency__tbl_item}>EUR</td>
-            <td className={styles.currency__tbl_item}>{eurBuy}</td>
-            <td className={styles.currency__tbl_item}>{eurSale}</td>
+            <td className={styles.currency__tbl_item}>5.6</td>
+            <td className={styles.currency__tbl_item}>5.6</td>
           </tr>
         </tbody>
       </table>
     </div>
   );
+
+  if (!currencyData) {
+    return <div>Loading...</div>;
+  }
+
+  const eurBuy = currencyData.eur.buy;
+  const eurSale = currencyData.eur.sale;
+  const USDSale = currencyData.usd.sale;
+  const USDBuy = currencyData.usd.buy;
+
+  // return (
+  //   <div className={styles.currency}>
+  //     <table className={styles.currency__tbl}>
+  //       <thead className={styles.currency__thead}>
+  //         <tr>
+  //           <th className={styles.currency__tbl_title}>Currency</th>
+  //           <th className={styles.currency__tbl_title}>Purchase</th>
+  //           <th className={styles.currency__tbl_title}>Sale</th>
+  //         </tr>
+  //       </thead>
+  //       <tbody className={styles.currency__tbody}>
+  //         <tr>
+  //           <td className={styles.currency__tbl_item}>USD</td>
+  //           <td className={styles.currency__tbl_item}>{USDBuy}</td>
+  //           <td className={styles.currency__tbl_item}>{USDSale}</td>
+  //         </tr>
+  //         <tr>
+  //           <td className={styles.currency__tbl_item}>EUR</td>
+  //           <td className={styles.currency__tbl_item}>{eurBuy}</td>
+  //           <td className={styles.currency__tbl_item}>{eurSale}</td>
+  //         </tr>
+  //       </tbody>
+  //     </table>
+  //   </div>
+  // );
 }
 
 export default Currency;
