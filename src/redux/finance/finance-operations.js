ef9f1f84d421177f4d84d24f50cced4ff05204ad
Alissa <alissa.werbach@live.de>
2023-04-25 11:30:10 +0200

    server

--- src/redux/finance/finance-operations.js
+++ src/redux/finance/finance-operations.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-//axios.defaults.baseURL = 'https://walletproject.onrender.com';
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://walletproject.onrender.com';
+//axios.defaults.baseURL = 'http://localhost:3000';
 
 export const getStatistics = createAsyncThunk(
   'finance/getStatistics',
